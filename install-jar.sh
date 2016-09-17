413b92ca36a5db6ae82e1f6f1331ce313a6f10fd
Travis Wellman <tw@traviswellman.com>
2016-09-16 18:37:45 -0700

    install-jar.sh can help referring to FXyz from gradle or maven

---
+++ install-jar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if ! [[ -v fxyz_version ]]
+then fxyz_version=2016.0.0.1
+fi
+
+# at the time of this writing, group and artifact are unofficially assumed to just be FXyz
+exec mvn install:install-file \
+     -DgroupId=FXyz \
+     -DartifactId=FXyz \
+     -Dpackaging=jar \
+     -Dversion=$fxyz_version \
+     -Dfile=dist/FXyzLib.jar \
+     -DgeneratePom=true
